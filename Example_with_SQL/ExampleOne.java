9722036ff2f0a6b8dbdd9be81c45b4b38352fd8f
Aliaksandr Komar <Komarthebest1992@gmail.com>
2024-07-27 22:53:32 +0300

    Seven eight part three. I write code in methods main

--- Example_with_SQL/ExampleOne.java
+++ Example_with_SQL/ExampleOne.java
@@ -3,5 +3,6 @@ package Example_with_SQL;
 public class ExampleOne {
     public static void main(String[] args) {
         System.out.println("My friend");
+        System.out.println("How's going?");
     }
 }
