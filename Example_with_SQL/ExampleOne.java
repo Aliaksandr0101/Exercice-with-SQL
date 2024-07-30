1a75f4f90967f0bdacd890918de40fce0ecd8b52
Aliaksandr Komar <Komarthebest1992@gmail.com>
2024-07-30 23:02:20 +0300

    Commit 10. I add code in method main.

--- Example_with_SQL/ExampleOne.java
+++ Example_with_SQL/ExampleOne.java
@@ -3,7 +3,8 @@ package Example_with_SQL;
 public class ExampleOne {
     public static void main(String[] args) {
         System.out.println("My friend");
-        System.out.println("How's going?");
+        System.out.println("How's it going?");
         System.out.println("Hello");
+        System.out.println("Break a lag");
     }
 }
