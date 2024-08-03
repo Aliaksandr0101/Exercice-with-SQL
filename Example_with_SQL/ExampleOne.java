e3c44a60e55827b335bcd0fd60892bea1b3491ea
Aliaksandr Komar <Komarthebest1992@gmail.com>
2024-08-03 23:22:37 +0300

    Commit 10 part two. I add code in method main.

--- Example_with_SQL/ExampleOne.java
+++ Example_with_SQL/ExampleOne.java
@@ -9,5 +9,6 @@ public class ExampleOne {
         System.out.println("Bless you");
         System.out.println("Bless you");
         System.out.println("Bless you");
+        System.out.println("Bless you");
     }
 }
