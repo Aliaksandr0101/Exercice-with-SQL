595002649dbd24452dd391a5e6693933c5d86098
Aliaksandr Komar <Komarthebest1992@gmail.com>
2024-08-12 22:44:15 +0300

    Commit 12. I change methods main. add sout

--- Example_with_SQL/ExampleOne.java
+++ Example_with_SQL/ExampleOne.java
@@ -8,8 +8,6 @@ public class ExampleOne {
         System.out.println("Break a lag");
         System.out.println("Bless you");
         System.out.println("We are people");
-        System.out.println("We are people");
-        System.out.println("We are people");
         for (int i = 3; i < 9; i++) {
             System.out.println(" i = " + i);}
     }
